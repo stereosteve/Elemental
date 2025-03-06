5466d84b8ad639ddb076bf8881d6dadeeaf4c1cb
Steve Perkins <stereosteve@users.noreply.github.com>
2025-03-06 09:37:27 -0500

    new setup

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { Home } from './routes/home'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+)
---
+++ src/routes/home.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
