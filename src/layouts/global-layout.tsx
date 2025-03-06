a6f2de231ac26f514e4b7c0ecb2d4dc8a2f5bb6e
Steve Perkins <stereosteve@users.noreply.github.com>
2025-03-06 11:37:40 -0500

    nav rail

--- src/layouts/global-layout.tsx
+++ src/layouts/global-layout.tsx
@@ -1,10 +1,30 @@
 import { Player } from '@/components/player'
-import { Outlet } from 'react-router'
+import { AudioWaveformIcon, HomeIcon } from 'lucide-react'
+import { NavLink, Outlet } from 'react-router'
+import './global-layout.css'
+
+const navItems = [
+  { to: '/', icon: <HomeIcon /> },
+  { to: '/explore/genres', icon: <AudioWaveformIcon /> },
+]
 
 export function GlobalLayout() {
   return (
     <div>
-      <Outlet />
+      <div className="nav-rail">
+        {navItems.map((i) => (
+          <NavLink
+            key={i.to}
+            to={i.to}
+            className="flex items-center justify-center w-16 h-16"
+          >
+            {i.icon}
+          </NavLink>
+        ))}
+      </div>
+      <div className="ml-16">
+        <Outlet />
+      </div>
       <Player />
     </div>
   )
