2d6ce4ff3d1b3c0154c00ae9f51c9b247b62b449
Steve Perkins <stereosteve@users.noreply.github.com>
2025-03-10 05:35:16 -0400

    nicer play queue ui

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,3 +1,5 @@
+import { hashid } from '@/lib/hashid'
+import { urlFor } from '@/lib/urlFor'
 import { useDJ } from '@/state/dj'
 import clsx from 'clsx'
 import { ListIcon } from 'lucide-react'
@@ -5,25 +7,30 @@ import { Link } from 'react-router'
 import { AudioPlayer } from './audio'
 import { CidImage } from './cid-image'
 import { Button } from './ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
 import { ScrollArea } from './ui/scroll-area'
-import {
-  Sheet,
-  SheetContent,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from './ui/sheet'
-import { hashid } from '@/lib/hashid'
-import { urlFor } from '@/lib/urlFor'
+import { useEffect, useRef, useState } from 'react'
 
 export function Player() {
+  const [isOpen, setIsOpen] = useState(false)
+  const nowPlayingRef = useRef<HTMLDivElement>(null)
   const dj = useDJ()
   const { track } = dj
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (isOpen && nowPlayingRef.current) {
+        nowPlayingRef.current.scrollIntoView()
+      }
+    })
+  }, [isOpen, nowPlayingRef])
+
   if (!track || !dj.djContext) return null
+
   const src = `https://discoveryprovider3.audius.co/v1/tracks/${hashid.encode(
     track.id
   )}/stream`
+
   return (
     <>
       <div style={{ height: 90 }}></div>
@@ -40,22 +47,22 @@ export function Player() {
           <AudioPlayer src={src} />
 
           <div>
-            <Sheet>
-              <SheetTrigger asChild>
+            <Popover onOpenChange={setIsOpen}>
+              <PopoverTrigger asChild>
                 <Button variant="outline">
                   <ListIcon />
                 </Button>
-              </SheetTrigger>
+              </PopoverTrigger>
 
-              <SheetContent>
-                <SheetHeader>
-                  <SheetTitle>Now Playing</SheetTitle>
-                </SheetHeader>
-                <ScrollArea className="flex-1 overflow-y-auto">
+              <PopoverContent className="w-[600px]" sideOffset={20}>
+                <ScrollArea className="flex-1 overflow-y-auto h-[60vh]">
                   {dj.djContext.items.map((item, idx) => {
-                    const itemClass = 'px-4 py-2'
+                    const itemClass = 'px-2 py-2 flex items-center gap-2'
                     return item.type == 'track' ? (
                       <div
+                        ref={
+                          dj.isPlaying({ track: item }) ? nowPlayingRef : null
+                        }
                         key={idx}
                         className={clsx(
                           itemClass,
@@ -63,22 +70,38 @@ export function Player() {
                         )}
                         onClick={() => dj.play(item)}
                       >
-                        {item.title}
+                        <CidImage img={item.img} size={48} />
+                        <div className="truncate whitespace-nowrap">
+                          <div className="font-bold">{item.title}</div>
+                          <div>{item.user.name}</div>
+                        </div>
                       </div>
                     ) : (
-                      <div key={idx} className={itemClass}>
+                      <div
+                        key={idx}
+                        className="mb-4"
+                        ref={
+                          dj.isPlaying({ playlist: item })
+                            ? nowPlayingRef
+                            : null
+                        }
+                      >
                         <div
-                          className="font-bold"
+                          className={itemClass}
                           onClick={() => dj.playPlaylist(item, item.tracks[0])}
                         >
-                          {item.name}
+                          <CidImage img={item.img} size={48} />
+                          <div className="truncate whitespace-nowrap">
+                            <div className="font-bold">{item.name}</div>
+                            <div>{item.user.name}</div>
+                          </div>
                         </div>
                         {item.tracks.map((track, idx) => (
                           <div
                             key={idx}
                             onClick={() => dj.playPlaylist(item, track)}
                             className={clsx(
-                              'ml-2',
+                              'ml-4 p-1',
                               dj.isPlaying({ playlist: item, track }) &&
                                 'bg-secondary'
                             )}
@@ -90,11 +113,8 @@ export function Player() {
                     )
                   })}
                 </ScrollArea>
-                <SheetFooter>
-                  <div className="bg-secondary p-4">Enjoy!</div>
-                </SheetFooter>
-              </SheetContent>
-            </Sheet>
+              </PopoverContent>
+            </Popover>
           </div>
         </div>
       </div>
